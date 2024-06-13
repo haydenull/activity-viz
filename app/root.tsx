66e86aa27c72b7fe2c56ad05927ca72e698a7733
Hayden Chen <hayden.chen.dev@gmail.com>
2024-06-13 11:21:05 +0800

    feat: :sparkles: completed basic layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,8 @@
 import type { LinksFunction } from '@remix-run/node'
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, Link, useLocation } from '@remix-run/react'
+import { Package2 } from 'lucide-react'
 
+import { cn } from './lib/utils'
 import styles from './root.css?url'
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
@@ -23,8 +25,56 @@ export function Layout({ children }: { children: React.ReactNode }) {
   )
 }
 
+const NavItem = ({ to, children, curPathname }: { to: string; children: React.ReactNode; curPathname: string }) => {
+  return (
+    <Link
+      to={to}
+      className={cn(
+        'transition-colors hover:text-foreground',
+        curPathname === to ? 'text-foreground' : 'text-muted-foreground',
+      )}
+    >
+      {children}
+    </Link>
+  )
+}
 export default function App() {
-  return <Outlet />
+  const location = useLocation()
+
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+            <Package2 className="h-6 w-6" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          <NavItem to="/dashboard" curPathname={location.pathname}>
+            Dashboard
+          </NavItem>
+          <NavItem to="/settings" curPathname={location.pathname}>
+            Settings
+          </NavItem>
+
+          {/* <Link to="/settings" className="text-muted-foreground transition-colors hover:text-foreground">
+            Settings
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Products
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Customers
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Analytics
+          </Link> */}
+        </nav>
+      </header>
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
 
 export function HydrateFallback() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from '@remix-run/node'
+import { Link, Outlet } from '@remix-run/react'
+import { Package2 } from 'lucide-react'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix SPA' }, { name: 'description', content: 'Welcome to Remix (SPA Mode)!' }]
@@ -6,20 +8,33 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1 className="text-rose-300">Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/guides/spa-mode" rel="noreferrer">
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+            <Package2 className="h-6 w-6" />
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          <Link to="/" className="text-foreground transition-colors hover:text-foreground">
+            Dashboard
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Settings
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Products
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Customers
+          </Link>
+          <Link to="/" className="text-muted-foreground transition-colors hover:text-foreground">
+            Analytics
+          </Link>
+        </nav>
+      </header>
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <Outlet />
+      </main>
     </div>
   )
 }
